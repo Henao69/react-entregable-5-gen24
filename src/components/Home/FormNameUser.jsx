6a0242a959229b55a9eb0c517197d64592da41ce
Sebastian Henao <henao7730@gmail.com>
2023-05-12 11:09:57 -0500

    Version 1.1 no me deja subirlo a Netlify

--- src/components/Home/FormNameUser.jsx
+++ src/components/Home/FormNameUser.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { setTrainerName } from "../../store/slices/trainerName.slice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import "./styles/formNameuser.css";
+// import "./styles/formNameuser.css";
 
 
 const FormNameUser = () => {
